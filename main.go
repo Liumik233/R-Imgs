04c8dbfa462c11d12897988c9048cd70cd675597
Liumik233 <liumikhazne@gmail.com>
2021-01-13 22:36:24 +0800

    update

--- main.go
+++ main.go
@@ -1,15 +1,47 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
+var url []string
+
+func readfile() {
+	f, e := os.Open("url.txt")
+	defer f.Close()
+	if e != nil {
+		fmt.Println("error:", e)
+	} else {
+		str := bufio.NewReader(f)
+		for {
+			r, err := str.ReadString('\n')
+			url = append(url, strings.TrimSpace(r))
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalln("readfile error:", err)
+			}
+		}
+	}
+}
 func papi(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Location", "https://od.liumik.tech/sjhl/Picture")
+	rand.Seed(time.Now().UnixNano())
+	i := rand.Intn(len(url) - 1)
+	log.Println(len(url))
+	w.Header().Add("Location", url[i])
 	w.WriteHeader(307)
 }
 func main() {
+	readfile()
 	http.HandleFunc("/papi", papi)
 	err := http.ListenAndServe(":1701", nil)
 	if err != nil {
